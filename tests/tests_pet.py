c52a2c17a53f00d52682f4df7989f173111376c3
st0xwork <st0x.work@proton.me>
2026-03-14 21:49:05 +0300

    Создал автотест на удаление несуществущего питомца

---
+++ tests/tests_pet.py
@@ -0,0 +1,19 @@
+from http.client import responses
+
+import allure
+import requests
+
+BASE_URL = "http://5.181.109.28:9090/api/v3"
+
+@allure.feature("Pet")
+class TestPet:
+    @allure.title("Попытка удалить несуществущего питомца")
+    def test_delete_nonexistent_pet(self):
+        with allure.step("Отправка запроса на удаление несуществущего питомца"):
+            response = requests.delete(url=f"{BASE_URL}/pet/9999")
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Проверка текстового содержимого ответа"):
+            assert response.text == "Pet deleted", "Текст ошибки не совпал с ожидаемым"
